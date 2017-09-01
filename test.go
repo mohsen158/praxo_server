3d2f9cf53465c222df08253c5000621d8326f292
mohsen <rahnaamei_158@yahoo.com>
2017-09-02 00:07:01 +0430

    update git

--- test.go
+++ test.go
@@ -2,7 +2,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var redis = require('redis');
- 
+ console.log("hi MOHSEN");
 server.listen(8890);
 io.on('connection', function (socket) {
  
